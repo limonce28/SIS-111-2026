4dd6c215aa4b11efc3a0455f771479de77de0e1a
Celeste Kjari <celestekjari@gmail.com>
2026-03-22 16:24:50 -0400

    actualice el archivo

--- TEMA1/typescript/mayormenor.ts
+++ TEMA1/typescript/mayormenor.ts
@@ -1,11 +1,24 @@
 import readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 const rl = readline.createInterface({ input, output }); 
-function mayormenor (a: number, b :number, c: number){
-    if (a> b && a > c) return '"Mayor : ${a}';
-    else if (b > a && b > c) return '"Mayor : {b}"' ;
-    else return ' Mayor :$ {c}"'
+
+function mayor(a: number, b: number, c: number){
+if(a > b && a > c) return `Mayor: ${a}`;
+else if(b > a && b > c) return `Mayor: ${b}`;
+else return `Mayor: ${c}`;
 }
 
-console.log ("****MAYOR MENOR****")
-const a = await rl.question
+function menor(a: number, b: number, c: number){
+if(a < b && a < c) return `Menor: ${a}`;
+else if(b < a && b < c) return `Menor: ${b}`;
+else return `Menor: ${c}`;
+}
+
+console.log("********Mayor Menor**********")
+const a = Number (await rl.question("Introduzca el valor de a: "))
+const b = Number (await rl.question("Introduzca el valor de b: "))
+const c = Number (await rl.question("Introduzca el valor de c: "))
+
+console.log(mayor(a,b,c));
+console.log(menor(a,b,c));
+rl.close();
